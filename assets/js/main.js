164b3765f4f29cbfb78e70df0e2d035e6995dd6c
DeonRMIT <65660085+DeonRMIT@users.noreply.github.com>
2020-05-24 03:55:57 +1000

    Major update
    Added report sections in.
    Collapsible headings.
    Custom CSS for said headings.
    Custom  JS for said headings.
    Bugfixes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,7 +64,21 @@
 				$nav_li.eq( ($nav_li.length / 2) ).addClass('is-middle');
 
 			}
+	//collapsing Headings
+			var coll = document.getElementsByClassName("collapsible");
+			var i;
 
+			for (i = 0; i < coll.length; i++) {
+			  coll[i].addEventListener("click", function() {
+			    this.classList.toggle("active");
+			    var content = this.nextElementSibling;
+			    if (content.style.display === "block") {
+			      content.style.display = "none";
+			    } else {
+			      content.style.display = "block";
+			    }
+			  });
+} 
 	// Main.
 		var	delay = 325,
 			locked = false;
